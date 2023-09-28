0a01c787df02ebbb0ebdad6a0b38366d95aa63a7
Irfan <irfansalim77@gmail.com>
2023-09-28 22:50:32 +0530

    Updated controllers

--- controllers/products.js
+++ controllers/products.js
@@ -1,9 +1,28 @@
+const Product = require("../models/product");
+
 const getAllProductsStatic = async (req, res) => {
-    res.status(200).json({ msg: "All Products Static" });
+    const product = await Product.find({});
+    res.status(200).json({ product });
 };
 
 const getAllProducts = async (req, res) => {
-    res.status(200).json({ msg: "All Products" });
+    const { featured, company, name } = req.query;
+    const queryObject = {};
+
+    if (featured) {
+        queryObject.featured = featured === "true" ? true : false;
+    }
+
+    if (company) {
+        queryObject.company = company;
+    }
+
+    if (name) {
+        queryObject.name = { $regex: name, $options: "i" };
+    }
+
+    const product = await Product.find(queryObject);
+    res.status(200).json({ product, nbhits: product.length });
 };
 
 module.exports = {
