18c687ee96e43d15ee911603908db34fe99a6de3
Irfan <irfansalim77@gmail.com>
2023-09-29 20:58:54 +0530

    Updated controller

--- controllers/products.js
+++ controllers/products.js
@@ -6,7 +6,7 @@ const getAllProductsStatic = async (req, res) => {
 };
 
 const getAllProducts = async (req, res) => {
-    const { featured, company, name } = req.query;
+    const { featured, company, name, sort, fields } = req.query;
     const queryObject = {};
 
     if (featured) {
@@ -21,8 +21,25 @@ const getAllProducts = async (req, res) => {
         queryObject.name = { $regex: name, $options: "i" };
     }
 
-    const product = await Product.find(queryObject);
-    res.status(200).json({ product, nbhits: product.length });
+    const result = Product.find(queryObject);
+
+    if (sort) {
+        const sortList = sort.split(",").join(" ");
+        result = result.sort(sortList);
+    } else {
+        result = result.sort("createDate");
+    }
+
+    if (fields) {
+        const fieldList = fields.split(",").join(" ");
+        result = result.select(fieldList);
+    }
+
+    const page = Number(req.query.page) || 1;
+    const limit = Number(req.query.limit) || 10;
+
+    const products = await result;
+    res.status(200).json({ products, nbhits: product.length });
 };
 
 module.exports = {
